67a5ceae48b64a699ce5db309001952f47ff893c
Mamed766 <mehemmeddemirli57@gmail.com>
2024-06-03 19:40:16 +0400

    js

---
+++ jss/script.js
@@ -0,0 +1,27 @@
+// task 1
+// const userVal = Number(prompt());
+
+// if (userVal % 33 > 30) {
+//   if (userVal % 14 > 10) {
+//     console.log(userVal % 5);
+//   }
+// }
+
+// task 2
+// const length = Number(prompt("Uzunluq uaz:"));
+// const width = Number(prompt("En yaz:"));
+
+// if (length > 10 && width > 10) {
+//   console.log(length * width, "sahesi");
+// } else if (length < 10 && width < 10) {
+//   console.log(2 * (length + width), "perimetri");
+// }
+
+// task 3
+const kvadrat = Number(prompt());
+
+const area = kvadrat * kvadrat;
+
+if (area > 25) {
+  console.log(area % 12);
+}
